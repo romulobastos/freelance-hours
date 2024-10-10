69908cb7fd774af31a8cd7939bd5308ac64c3f61
Rômulo Bastos <romulobastos.design@gmail.com>
2024-10-10 14:38:39 -0300

    :sparkles: feat(db): add new migration and database factory
    - created migration for 'proposals'
    
    - added factory for 'Proposal' model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,8 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // fabricar 200 usuários
         User::factory(200)->create();
+
+        // fabricar projetos e propostas
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
+            ->each(function (User $u) {
+
+                // fabricar 10 projetos
+                // armazena em $project cada projeto criado
+                // envia o id do usuário, recebido no "each", para a criação da fk
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                // fabricar de 2 a 15 propostas para cada projeto
+                // envia o id do projeto, armazenado acima, para a criação da fk
+                Proposal::factory()->count(random_int(2,15))->create(['project_id' => $project->id]);
+            });
     }
 }
