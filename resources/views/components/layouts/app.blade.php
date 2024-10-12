0f4096b39946616441af27eef4a31b0847b99b69
Rômulo Bastos <romulobastos.design@gmail.com>
2024-10-12 02:56:52 -0300

    :memo: docs: update "readme" file and app title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,9 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<title>{{ $title ?? 'Page Title' }}</title>
+		<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
+        <title>{{ config('app.name') ?? 'Page Title' }}</title>
 		@vite(['resources/css/app.css', 'resources/js/app.js'])
 	</head>
 	<body class="bg-[#070715] flex flex-col min-h-full text-[#C3C3D1]">
