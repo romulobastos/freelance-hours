d72f16ee5c38f8b5967e027e9ff9cd0816200e8d
Rômulo Bastos <romulobastos.design@gmail.com>
2024-10-12 00:43:55 -0300

    :sparkles: feat: implement email notifications - notify project owner when a new proposal is submitted - notify proposal owner if their position drops in the ranking

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -41,6 +43,8 @@ class Create extends Component
             $this->arrangePositions($proposal);
         });
 
+        $this->project->author->notify(new NewProposal($this->project));
+
         $this->dispatch('proposal::created');
         $this->modal = false;
     }
@@ -58,7 +62,9 @@ class Create extends Component
 
         if($otherProposal) {
             $proposal->update(['position_status' => 'up']);
-            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+            $oProposal = Proposal::find($otherProposal->id);
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
         }
 
         // ajusta posicoes e status (up or down) na lista
